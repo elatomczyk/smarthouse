8f7bd009998405d4fc7d4d425ad5330be1fb31c3
ela <et17@onet.pl>
2015-01-05 02:00:11 +0100

    Comments in url conf

--- SmartHouse/urls.py
+++ SmartHouse/urls.py
@@ -17,6 +17,6 @@ urlpatterns = patterns('',
     #register
     url(r'^register/$', 'account.views.register_user', name='register'),
     url(r'^register/register_success/$', 'account.views.register_success'),
-    #wskaznik temperatury
+    #temperature and humidity scope
     url(r'^scope/$',views.create_scope, name="scope")
 )
