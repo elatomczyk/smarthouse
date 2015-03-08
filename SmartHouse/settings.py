160df2c0483ef28ae67ea7918dac0d642fee5add
Agnieszka Celińska <a.celinska@yahoo.pl>
2015-03-09 00:52:46 +0100

    deleted unnecessary folders

--- SmartHouse/settings.py
+++ SmartHouse/settings.py
@@ -26,6 +26,7 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
 # Application definition
 
 INSTALLED_APPS = (
@@ -67,8 +68,10 @@ EMAIL_PORT = 587
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER': 'smarthouse',
+        'PASSWORD': 'smart',
     }
 }
 
